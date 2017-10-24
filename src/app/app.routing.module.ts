e6c9aafe731d893e092e045187b54ca88767d7a1
Divya <divya_nehrudurai@homedepot.com>
2017-10-24 16:06:46 -0400

    Added poc-page

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { RegistrationPageComponent } from './registration-page/registration-page.component';
 import { LoginComponent } from './Login/login.component';
+import { POCPageComponent } from './poc-page/poc-page.component';
 
 const appRoutes: Routes = [
     {   path: 'app-registration-page', component : RegistrationPageComponent },
     {   path: 'app-login', component: LoginComponent },
-    { path: '', redirectTo : '/app-login', pathMatch : 'full'}
+    { path: '', redirectTo : '/app-login', pathMatch : 'full'},
+    { path: 'app-poc-page', component: POCPageComponent}
   ];
 
   @NgModule({
@@ -17,5 +19,5 @@ const appRoutes: Routes = [
   })
 
   export class AppRoutingModule {  }
-  export const routingComponents = [LoginComponent, RegistrationPageComponent];
+  export const routingComponents = [LoginComponent, RegistrationPageComponent, POCPageComponent];
   
---
+++ src/app/poc-page/poc-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { EmployeeService } from '../services/employee.service'
+
+@Component({
+    selector: 'app-poc-page',
+    templateUrl: './poc-page.component.html',
+    
+  })
+
+  export class POCPageComponent {
+    
+  }
